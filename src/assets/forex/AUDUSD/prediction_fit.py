36402543fe5bbd043000c869d9c934f79cc8beac
kyle-black <kpblack87@gmail.com>
2024-01-16 13:03:57 -0600

    fix file structure

--- src/assets/forex/AUDUSD/prediction_fit.py
+++ src/assets/forex/AUDUSD/prediction_fit.py
@@ -31,9 +31,9 @@ def make_predictions(symbol, new_data: pd.DataFrame) -> pd.Series:
     #scaler = joblib.load(f'models/scaler_EURUSD.pkl')
 
 
-    clf = joblib.load(f'models/random_forest_model_up_EURUSD_60.pkl')
-    pca = joblib.load(f'models/pca_transformation_up_EURUSD_60.pkl')
-    scaler = joblib.load(f'models/scaler_EURUSD.pkl')
+    clf = joblib.load(f'models/random_forest_model_up_AUDUSD_60.pkl')
+    pca = joblib.load(f'models/pca_transformation_up_AUDUSD_60.pkl')
+    scaler = joblib.load(f'models/scaler_AUDUSD.pkl')
     
     # Prepare the new data
     #feature_cols = ['Close','High','Low','Open','Volume','Daily_Returns', 'Middle_Band', 'Upper_Band', 'Lower_Band',
