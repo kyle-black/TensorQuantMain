007069de4fa10a6908ddbd22e3a30c960645544d
kyle-black <kpblack87@gmail.com>
2024-01-19 11:02:30 -0600

    updated USDCAD Training

--- src/assets/forex/training/USDCAD/train_models.py
+++ src/assets/forex/training/USDCAD/train_models.py
@@ -205,9 +205,9 @@ def random_forest_classifier(df, asset):
 
     # After processing all splits, compute overall metrics
     
-    joblib.dump(clf, 'models/random_forest_model_up_CADUSD_60.pkl')
-    joblib.dump(pca, 'models/pca_transformation_up_CADUSD_60.pkl')
-    joblib.dump(scaler, 'models/scaler_GBPUSD.pkl')
+    joblib.dump(clf, '../predictions/models/random_forest_model_up_CADUSD_60.pkl')
+    joblib.dump(pca, '../predictions/models/pca_transformation_up_CADUSD_60.pkl')
+    joblib.dump(scaler, '../predictions/models/scaler_CADUSD.pkl')
 
     for actual,prediction,dwn,neutral,up in zip(y_test,y_pred,probas[:,0],probas[:,1], probas[:,2]):
         print(actual, prediction, dwn, neutral,up)
