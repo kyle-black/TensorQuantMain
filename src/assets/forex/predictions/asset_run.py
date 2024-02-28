1635b061510c2ab9e5316e25778c8b64f7a1a3fd
kyle-black <kpblack87@gmail.com>
2024-02-28 12:42:18 -0600

    updated testing

--- src/assets/forex/predictions/asset_run.py
+++ src/assets/forex/predictions/asset_run.py
@@ -5,13 +5,13 @@ import time
 
 
 def asset_(asset_list):
-
-    for asset in asset_list:
-        prediction_oop.run_predictions(asset)
-        print('Asset:', asset)
+    prediction_oop.run_predictions(asset_list)
+    #for asset in asset_list:
+     #   prediction_oop.run_predictions(asset)
+      #  print('Asset:', asset)
        # time.sleep(1)
 
 if __name__ in "__main__":
-    asset_list = ['EURUSD']
+    asset_list = 'AUDUSD'
     
     asset_(asset_list)
