4935c22572685ad9a6e906b4058220ba69af08b9
kyle-black <kpblack87@gmail.com>
2024-02-29 11:28:08 -0600

    fix list iteration for asset

--- src/assets/forex/predictions/asset_run.py
+++ src/assets/forex/predictions/asset_run.py
@@ -4,14 +4,14 @@ import time
 
 
 
-def asset_(asset_list):
-    prediction_oop.run_predictions(asset_list)
+def asset_(asset_n):
+    prediction_oop.run_predictions(asset_n)
     #for asset in asset_list:
      #   prediction_oop.run_predictions(asset)
       #  print('Asset:', asset)
        # time.sleep(1)
 
 if __name__ in "__main__":
-    asset_list = ['AUDUSD']
+    asset_name = 'AUDUSD'
     
-    asset_(asset_list)
+    asset_(asset_name)
