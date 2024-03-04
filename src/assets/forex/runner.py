1a07e4083d2f1db4777fffd38718758ee7775dfd
kyle-black <kpblack87@gmail.com>
2024-03-04 12:24:39 -0600

    updated runner

---
+++ src/assets/forex/runner.py
@@ -0,0 +1,11 @@
+import sys
+forex_pairs = ['AUDUSD', 'GBPUSD', 'USDCHF', 'USDHKD', 'EURUSD', 'USDCAD', 'USDCNH', 'USDJPY']
+
+# Add each forex pair's directory to the Python path
+for pair in forex_pairs:
+    sys.path.append(f'/path/to/{pair}')
+
+# Import the prediction_oop module from each forex pair's directory and run the runner function
+for pair in forex_pairs:
+    module = __import__(pair + '.prediction_oop', fromlist=[pair])
+    module.runner(pair)
