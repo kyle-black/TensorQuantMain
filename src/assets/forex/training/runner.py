000e24f51e97746ad552d2c8b9ea58e07fddd3ca
kyle-black <kpblack87@gmail.com>
2024-03-07 16:28:03 -0600

    updated runner EURUSD

--- src/assets/forex/training/runner.py
+++ src/assets/forex/training/runner.py
@@ -6,7 +6,7 @@ print(current_directory)
 
 #forex_pairs = ['AUDUSD','USDJPY', 'NZDUSD', 'USDCAD', 'USDCHF']
 #forex_pairs = ['AUDUSD','USDJPY', 'NZDUSD', 'USDCAD', 'USDCHF']
-forex_pairs = ['GBPUSD']
+forex_pairs = ['EURUSD']
 
 #### Tested: 'AUDUSD','EURUSD', 'GBPUSD'
 #### not working: 'EURUSD' 'GBPUSD'
