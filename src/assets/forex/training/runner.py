95c4a7a131c1dbe5f730af2680327e164ade2ccf
kyle-black <kpblack87@gmail.com>
2024-03-07 16:37:58 -0600

    USDHKG training

--- src/assets/forex/training/runner.py
+++ src/assets/forex/training/runner.py
@@ -6,7 +6,7 @@ print(current_directory)
 
 #forex_pairs = ['AUDUSD','USDJPY', 'NZDUSD', 'USDCAD', 'USDCHF']
 #forex_pairs = ['AUDUSD','USDJPY', 'NZDUSD', 'USDCAD', 'USDCHF']
-forex_pairs = ['EURUSD']
+forex_pairs = ['USDHKD']
 
 #### Tested: 'AUDUSD','EURUSD', 'GBPUSD'
 #### not working: 'EURUSD' 'GBPUSD'
