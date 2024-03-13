745064b78a0413758ddaa3712ee2397bad53bcfd
kyle-black <kpblack87@gmail.com>
2024-03-13 11:34:22 -0500

    updated

--- src/assets/forex/training/runner.py
+++ src/assets/forex/training/runner.py
@@ -15,9 +15,13 @@ original_directory = os.getcwd()
 # Add each forex pair's directory to the Python path
 for pair in forex_pairs:
     os.chdir(os.path.join(current_directory, pair))
+    print('first part:',os.getcwd())
+    
+    
     module = __import__(pair + '.prediction_oop', fromlist=[pair])
-    module.runner(pair)
+   # module.runner(pair)
     os.chdir(original_directory)
+    print('second part:',os.getcwd())
 # Import the prediction_oop module from each forex pair's directory and run the runner function
 
 '''
