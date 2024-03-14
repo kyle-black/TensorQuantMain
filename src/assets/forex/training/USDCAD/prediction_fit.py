b515108d032989750ee2c780bdb5ae5348ea16de
kyle-black <kpblack87@gmail.com>
2024-03-14 12:46:46 -0500

    updated file path print out

--- src/assets/forex/training/USDCAD/prediction_fit.py
+++ src/assets/forex/training/USDCAD/prediction_fit.py
@@ -30,7 +30,9 @@ def make_predictions(symbol, new_data: pd.DataFrame) -> pd.Series:
     #clf = joblib.load(f'../{symbol}_models/random_forest_model_up_{symbol}.pkl')
     #pca = joblib.load(f'../{symbol}_models/pca_transformation_up_{symbol}.pkl')
     #scaler = joblib.load(f'../{symbol}_models/scaler_{symbol}.pkl')
-
+    print('updated file path:', os.path.join(os.path.dirname(os.path.abspath(__file__)), f'{symbol}_models/random_forest_model_up_{symbol}_60.pkl'))
+   
+   
     clf = joblib.load(os.path.join(os.path.dirname(os.path.abspath(__file__)), f'{symbol}_models/random_forest_model_up_{symbol}_60.pkl'))
     pca = joblib.load(os.path.join(os.path.dirname(os.path.abspath(__file__)), f'{symbol}_models/pca_transformation_up_{symbol}_60.pkl'))
     scaler = joblib.load(os.path.join(os.path.dirname(os.path.abspath(__file__)), f'{symbol}_models/scaler_{symbol}.pkl'))
