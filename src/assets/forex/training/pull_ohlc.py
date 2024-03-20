27fb56246512b26d6d8633f1025cb4f7cdfa108a
kyle-black <kpblack87@gmail.com>
2024-03-20 12:04:50 -0500

    add pull ohlc to project

--- src/assets/forex/training/pull_ohlc.py
+++ src/assets/forex/training/pull_ohlc.py
@@ -2,7 +2,21 @@ from tvDatafeed import TvDatafeed, Interval
 import pandas as pd
 import datetime
 import json
-from redis_connect import url_connection
+#from redis_connect import url_connection
+
+
+import redis
+#from urllib.parse import urlparse  
+
+
+
+REDIS_URL = "redis://default:zHeoOL4uqpzaxTC7YgtuWvq4HRNSsoD0@redis-17905.c326.us-east-1-3.ec2.cloud.redislabs.com:17905"
+
+# Create a Redis connection
+url_connection = redis.from_url(REDIS_URL)
+
+
+
 
 def latest_data_5(security):
     username = 'StoCASHtic-ML'
@@ -20,6 +34,9 @@ def latest_data_5(security):
 
     df =df.reset_index()
     df.rename(columns = {'datetime':'Date','open': 'Open', 'high':'High', 'low':'Low', 'close':'Close','volume':'Volume'}, inplace=True)
+
+    json_str = df.to_string(orient='split')
+    url_connection.set(f'{security}_5m', json_str)
 
    
     return df
@@ -42,6 +59,8 @@ def latest_data_15(security):
     df =df.reset_index()
     df.rename(columns = {'datetime':'Date','open': 'Open', 'high':'High', 'low':'Low', 'close':'Close','volume':'Volume'}, inplace=True)
 
+    json_str = df.to_string(orient='split')
+    url_connection.set(f'{security}_15m', json_str)
     
     return df
 
@@ -60,6 +79,8 @@ def latest_data_30(security):
     df =df.reset_index()
     df.rename(columns = {'datetime':'Date','open': 'Open', 'high':'High', 'low':'Low', 'close':'Close','volume':'Volume'}, inplace=True)
 
+    json_str = df.to_string(orient='split')
+    url_connection.set(f'{security}_30m', json_str)
    
     return df
 
@@ -71,14 +92,48 @@ def latest_data_60(security):
 
     nifty_index_data = tv.get_hist(symbol=f'{security}',exchange='OANDA',interval=Interval.in_1_hour,n_bars=1000)
 
+
     df= nifty_index_data
 
     df =df.reset_index()
     df.rename(columns = {'datetime':'Date','open': 'Open', 'high':'High', 'low':'Low', 'close':'Close','volume':'Volume'}, inplace=True)
 
+    #json_str = df.to_string(orient='split')
     json_str = df.to_json(orient='split')
     
     url_connection.set(f'{security}_60m', json_str)
 
+   # for entry in entries:
+   #     print('Redis update:',entry)
 
     return df
+
+
+
+if __name__ == "__main__":
+    latest_data_5('EURUSD')
+    latest_data_15('EURUSD')
+    latest_data_30('EURUSD')
+    latest_data_60('EURUSD')
+    latest_data_5('AUDUSD')
+    latest_data_15('AUDUSD')
+    latest_data_30('AUDUSD')
+    latest_data_60('AUDUSD')
+    latest_data_5('GBPUSD')
+    latest_data_15('GBPUSD')
+    latest_data_30('GBPUSD')
+    latest_data_60('GBPUSD')
+    latest_data_5('USDJPY')
+    latest_data_15('USDJPY')
+    latest_data_30('USDJPY')
+    latest_data_60('USDJPY')
+    latest_data_5('USDCHF')
+    latest_data_15('USDCHF')
+    latest_data_30('USDCHF')
+    latest_data_60('USDCHF')
+    latest_data_5('USDCAD')
+    latest_data_15('USDCAD')
+    latest_data_30('USDCAD')
+    latest_data_60('USDCAD')
+    
+    
