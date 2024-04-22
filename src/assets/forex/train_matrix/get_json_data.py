509cefdfeea396e83076961b4c6d3988925da384
kyle-black <kpblack87@gmail.com>
2024-04-22 14:14:13 -0500

    updated sympbol list

--- src/assets/forex/train_matrix/get_json_data.py
+++ src/assets/forex/train_matrix/get_json_data.py
@@ -57,6 +57,6 @@ if __name__ == "__main__":
    #get_json_from_url(symbol_list=['EURUSD'])#'GBPUSD','USDJPY','USDCHF','USDCAD','AUDUSD','NZDUSD','EURGBP','EURJPY','GBPJPY','AUDJPY','NZDJPY','USDHKD'])
 
 
-    symbol_list = ['EURUSD']
+    symbol_list = ['EURUSD','GBPUSD']
     main(symbol_list)
   
