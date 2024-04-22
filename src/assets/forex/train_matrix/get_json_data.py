0732e60df8b5b0210bc4583ed13a89dcf897b09a
kyle-black <kpblack87@gmail.com>
2024-04-22 14:24:34 -0500

    fix threadeing symbols

--- src/assets/forex/train_matrix/get_json_data.py
+++ src/assets/forex/train_matrix/get_json_data.py
@@ -4,47 +4,47 @@ from datetime import datetime, timedelta
 import time
 from concurrent.futures import ThreadPoolExecutor
 
-def get_json_from_url(symbol_list):
+def get_json_from_url(symbol):
     start_date = datetime.strptime('2010-01-01', '%Y-%m-%d')
     end_date = start_date + timedelta(days=10)
     final_date = datetime.strptime('2023-01-01', '%Y-%m-%d')
 
     execution_counter = 0
 
-    for symbol in symbol_list:
-        while start_date <= final_date:
-            url = f"https://financialmodelingprep.com/api/v3/historical-chart/5min/{symbol}?from={start_date.strftime('%Y-%m-%d')}&to={end_date.strftime('%Y-%m-%d')}&apikey=3e17d2b777a13feee4c1243985cdc7c4"
-            response = requests.get(url)
-            data = response.json()
+    #for symbol in symbol_list:
+    while start_date <= final_date:
+        url = f"https://financialmodelingprep.com/api/v3/historical-chart/5min/{symbol}?from={start_date.strftime('%Y-%m-%d')}&to={end_date.strftime('%Y-%m-%d')}&apikey=3e17d2b777a13feee4c1243985cdc7c4"
+        response = requests.get(url)
+        data = response.json()
 
-            print(f'retreving {symbol} start:{start_date} end:{end_date}')
+        print(f'retreving {symbol} start:{start_date} end:{end_date}')
 
-            # Save or append data to JSON file
-            try:
-                with open(f'updated_data/{symbol}.json', 'r') as f:
-                    if f.read().strip():  # Check if file is not empty
-                        f.seek(0)  # Reset file pointer to beginning
-                        existing_data = json.load(f)
-                        combined_data = existing_data + data
-                    else:
-                        combined_data = data
-            except FileNotFoundError:
-                combined_data = data
+        # Save or append data to JSON file
+        try:
+            with open(f'updated_data/{symbol}.json', 'r') as f:
+                if f.read().strip():  # Check if file is not empty
+                    f.seek(0)  # Reset file pointer to beginning
+                    existing_data = json.load(f)
+                    combined_data = existing_data + data
+                else:
+                    combined_data = data
+        except FileNotFoundError:
+            combined_data = data
 
-            with open(f'updated_data/{symbol}.json', 'w') as f:
-                json.dump(combined_data, f)
+        with open(f'updated_data/{symbol}.json', 'w') as f:
+            json.dump(combined_data, f)
 
-            # Increment dates by 10 days
-            start_date += timedelta(days=10)
-            end_date += timedelta(days=10)
+        # Increment dates by 10 days
+        start_date += timedelta(days=10)
+        end_date += timedelta(days=10)
 
-            # Increment execution counter
-            execution_counter += 1
+        # Increment execution counter
+        execution_counter += 1
 
-            # If execution counter hits 240, pause for 60 seconds and reset counter
-            if execution_counter >= 240:
-                time.sleep(60)
-                execution_counter = 0
+        # If execution counter hits 240, pause for 60 seconds and reset counter
+        if execution_counter >= 240:
+            time.sleep(60)
+            execution_counter = 0
 
 
 def main(symbol_list):
