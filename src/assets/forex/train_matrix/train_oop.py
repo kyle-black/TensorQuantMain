75cfa00a0bc953d06eb9ce809192bf817e513959
kyle-black <kpblack87@gmail.com>
2024-04-23 15:26:47 -0500

    updated dollar bar

--- src/assets/forex/train_matrix/train_oop.py
+++ src/assets/forex/train_matrix/train_oop.py
@@ -43,7 +43,7 @@ class CreateBars:
         # Check if time_bar_df has been created, if not, create it
         if self.time_bar_df is None:
             self.time_bars()
-        return bc.get_dollar_bars(self.time_bar_df, 1000, self.asset)
+        return bc.get_dollar_bars(self.time_bar_df, 10000, self.asset)
     
     
 class Analysis:
