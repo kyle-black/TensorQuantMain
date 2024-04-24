9b09e3a65ca77774aa1fb9289bba51a87fea00e3
kyle-black <kpblack87@gmail.com>
2024-04-24 13:32:40 -0500

    updated volume bars

--- src/assets/forex/train_matrix/train_oop.py
+++ src/assets/forex/train_matrix/train_oop.py
@@ -36,7 +36,7 @@ class CreateBars:
         # Check if time_bar_df has been created, if not, create it
         if self.time_bar_df is None:
             self.time_bars()
-        return bc.get_volume_bars(self.time_bar_df, 100000, self.asset)
+        return bc.get_volume_bars(self.time_bar_df, 1000, self.asset)
     
 
     def dollar_bars(self):
@@ -184,6 +184,8 @@ if __name__ == "__main__":
     
     volume_bars_df = bar_creator.vol_bars()
     print(volume_bars_df)
+
+    volume_bars_df.to_csv('volume_bars.csv')
     #dollar_bars_df = bar_creator.dollar_bars()
 
     '''
