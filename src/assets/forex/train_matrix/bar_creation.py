408f53dea0e06cf25636e471398a0e4c617d43e7
kyle-black <kpblack87@gmail.com>
2024-04-24 15:15:55 -0500

    percantage chang

--- src/assets/forex/train_matrix/bar_creation.py
+++ src/assets/forex/train_matrix/bar_creation.py
@@ -59,7 +59,7 @@ def get_volume_bars(df, volume_threshold):
 
     # Convert the list of bars to a DataFrame
     volume_bars_df = pd.DataFrame(volume_bars).set_index('Date')
-    volume_bars_df['Daily_Returns'] = volume_bars_df['Close'].pct_change()
+    volume_bars_df['Daily_Returns'] = (volume_bars_df['Close'].pct_change() *100)
 
     return volume_bars_df
 
