51f505d16754a128a1f151ea6fae380a6920e70d
kyle-black <kpblack87@gmail.com>
2024-04-23 20:08:34 -0500

    test 10000

--- src/assets/forex/train_matrix/train_models.py
+++ src/assets/forex/train_matrix/train_models.py
@@ -160,7 +160,7 @@ def random_forest_classifier(df, asset, lookback):
 
     # Initialize GridSearchCV
     #clf = SVC(probability=True, C=50)
-    clf =RandomForestClassifier( random_state=20, n_estimators=1000, class_weight='balanced')
+    clf =RandomForestClassifier( random_state=20, n_estimators=10000, class_weight='balanced')
 
     #grid_search = GridSearchCV(estimator=clf, param_grid=param_grid, cv=3, n_jobs=-1, verbose=2)
     clf.fit(X_train, y_train, sample_weight=weight_data)
