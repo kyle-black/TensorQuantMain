0887971c4e6405c08eb05e6266ca5d7d98e67373
kyle-black <kpblack87@gmail.com>
2024-05-13 15:30:25 -0500

    vol bars

--- src/assets/forex/train_matrix/dollar_bars.py
+++ src/assets/forex/train_matrix/dollar_bars.py
@@ -27,13 +27,13 @@ def dollar_bar_creator(asset,dollar_amt):
     for i, d in df_.iterrows():
 
 
-        dollar_count+=round((d[volume] * d[close]),2)
+        #dollar_count+=round((d[volume] * d[close]),2)
+        vol_count += d[volume]
 
-
-        if dollar_count >= dollar_amt:
+        if vol_count >= dollar_amt:
             bar ={'Date':i,'Close':d[close],'Volume':d[volume],'AUDUSD':d['AUDUSD_Close'],'USDCAD':d['USDCAD_Close']}
             new_bar.append(bar)
-            dollar_count = 0
+            #dollar_count = 0
          
         
 
