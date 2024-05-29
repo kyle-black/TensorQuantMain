f98d7ee19208d8ae24fc05186e6a153b908d0753
kyle-black <kpblack87@gmail.com>
2024-05-29 13:57:36 -0500

    updated add_merged function

--- src/assets/forex/train_matrix/insert_db2.py
+++ src/assets/forex/train_matrix/insert_db2.py
@@ -72,18 +72,39 @@ def data_pull():
     durable['Date'] = pd.to_datetime(durable['Date'])
     merged = pd.merge_asof(merged, durable, on='Date', direction='nearest')
     merged['durableGoods'] = merged['durableGoods'].fillna(method='ffill')
-    
     ########
 
+    
     merged.to_csv('merged.csv')
     
-    return merged     
+    return merged  
+
+def add_merged():
+
+    indicator_list = ['15YR_Fixed','30YR_Fixed','CPI','GDP','Production_Total_Index','Yields_COD','consumerSentiment','federalFunds','inflation','inflationRate','initialClaims','nominalPotentialGDP','rates_CreditCards','realGDP','realGDPPerCapita', 'retailMoneyFunds', 'retailSales']
+
+    engine = create_engine('mysql+mysqlconnector://doadmin:AVNS_oW0kYA-LJsBz5pksVi4@tq-training-data-do-user-13042543-0.c.db.ondigitalocean.com:25060/defaultdb')
+     
+    for i in indicator_list:
+        data = pd.read_sql(f'SELECT date as Date, value as {i} FROM {i}', engine)
+        data['Date'] = pd.to_datetime(data['Date'])
+        merged = pd.merge_asof(merged, data, on='Date', direction='nearest')
+        merged[i] = merged[i].fillna(method='ffill')
+
+    merged.to_csv('merged.csv')
+
+    return merged
+    
+
 
 
 
 if __name__ == "__main__":
 
-    print(data_pull())
+    #print(data_pull())
+    
+    print(add_merged())
+    
     '''
     path = 'updated_data/econ'
     dir_list = os.listdir(path)
