d35172e2a9c31fa6571e7eb082e06df53ce07267
kyle-black <kpblack87@gmail.com>
2024-06-05 12:10:20 -0500

    updated parqut

--- src/assets/forex/train_matrix/oop_train.py
+++ src/assets/forex/train_matrix/oop_train.py
@@ -222,7 +222,7 @@ def prepare_data():
     asset = 'EURUSD'
     dollar_amount =1000
     lookback = 60
-
+    '''
     # Use Dask to read the CSV file in chunks
     raw = dd.read_csv('merged.csv')
 
@@ -235,7 +235,9 @@ def prepare_data():
 
     # Save the DataFrame in a more efficient format
     df.to_parquet('inf_check.parquet')
+    '''
     
+    df = pd.read_parquet('inf_check.parquet')
     L = Labeling(df,asset, lookback)
     print('Applying Triple Barriers:')
 
