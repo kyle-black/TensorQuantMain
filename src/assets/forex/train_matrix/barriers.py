4d31a788252d56dea366c134ab5a462357fba64a
kyle-black <kpblack87@gmail.com>
2024-06-12 14:02:50 -0500

    updated unix

--- src/assets/forex/train_matrix/barriers.py
+++ src/assets/forex/train_matrix/barriers.py
@@ -208,8 +208,8 @@ def apply_triple_barrier_P(df, pt_sl, num_days_active):
 
 
 
-def calculate_barriers_R(df,lookback):
+def calculate_barriers_R(df, lookback):
+    df['Date'] = pd.to_datetime(df['Date'])
     df['unix'] = df['Date'].astype('int64') // 10**9
-    
     return df
 
