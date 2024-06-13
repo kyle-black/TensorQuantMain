782383a70845c8164b7ef764f36ffc7a70535cb0
kyle-black <kpblack87@gmail.com>
2024-06-13 15:16:19 -0500

    updated

--- src/assets/forex/train_matrix/train_models.py
+++ src/assets/forex/train_matrix/train_models.py
@@ -84,7 +84,7 @@ def ensemble_methods(df, asset, lookback):
     #df = df.dropna(how='all')
     #df = df[lookback:]
     # Drop unnecessary columns early and use inplace=True
-    df.drop(columns=['touch_lower', 'touch_upper', 'Date', 'Close', 'Volume', 'upper_barrier', 'lower_barrier', 'pct_change'], inplace=True)
+    df.drop(columns=[  'Date','unix','endbarrier_unix', 'Close', 'Volume', 'upper_barrier', 'lower_barrier', 'pct_change', 'Datetime'], inplace=True)
     df.dropna(how='all', inplace=True)
     df = df[lookback:]
     # Splitting data
