c2e880d6faf1399459e9bb2970c232546bf0749a
kyle-black <kpblack87@gmail.com>
2024-06-17 15:33:38 -0500

    updated barrier days

--- src/assets/forex/train_matrix/barriers.py
+++ src/assets/forex/train_matrix/barriers.py
@@ -63,7 +63,7 @@ def new_apply_triple_barrier(df, pt_sl, endbar, asset):
         barriers.at[timestamp, 'upper_barrier'] = upper_barrier
         barriers.at[timestamp, 'lower_barrier'] = lower_barrier
 
-        t1_date = timestamp + pd.Timedelta(hours=72)
+        t1_date = timestamp + pd.Timedelta(days=30)
         t1_date = min(t1_date, df.index[-1])
         
         barriers.at[timestamp, 't1'] = t1_date
