7292d0edbfa87c5cb9ad0459908980be84861ec1
kyle-black <kpblack87@gmail.com>
2024-06-18 15:59:41 -0500

    updated drop unix

--- src/assets/forex/train_matrix/train_models.py
+++ src/assets/forex/train_matrix/train_models.py
@@ -83,7 +83,7 @@ def ensemble_methods(df, asset, lookback):
     threshold = 0.7 
 
 
-    
+    df.drop
     #df = df.drop(columns=['touch_lower', 'touch_upper'])
     #df = df.dropna(how='all')
     #df = df[lookback:]
@@ -93,9 +93,9 @@ def ensemble_methods(df, asset, lookback):
     #df['endbarrier_time'] = df['endbarrier_unix']
     #df['endbarrier_time'] = pd.to_datetime(df['endbarrier_unix'], unit='s')
     
-   
+    df = df.drop(df.columns[-5], axis=1)
 
-    print('endbarrier',df)
+    #print('endbarrier',df.columns)
 
     df['endbarrier_unix'] = pd.to_datetime(df['endbarrier_unix'], unit='s')
     df['unix'] = pd.to_datetime(df['unix'], unit='s')
