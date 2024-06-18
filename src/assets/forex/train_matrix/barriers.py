371abbaef2c061ed0fab0526e564d5c7e7607578
kyle-black <kpblack87@gmail.com>
2024-06-18 12:48:11 -0500

    updated touch prices

--- src/assets/forex/train_matrix/barriers.py
+++ src/assets/forex/train_matrix/barriers.py
@@ -209,41 +209,65 @@ def apply_triple_barrier_P(df, pt_sl, num_days_active):
 
 
 def calculate_barriers_R(df, lookback):
-    date_index = df.index
     volatility = df['Close'].pct_change().rolling(window=1000).std()
+    #volatility = df['Close'].pct_change().std()
+    date_index = df.index
+    
     df['Datetime'] = pd.to_datetime(df['Date'])
     df['unix'] = df['Datetime'].astype('int64') // 10**9
 
-    df['upper_barrier'] = df['Close'] * (1 + (1*volatility))
+
+
+    df['upper_barrier'] = df['Close'] * (1 +1 * (1*volatility))
     df['lower_barrier'] = df['Close'] * (1 -1 * (1*volatility))
 
     lookback_hours = pd.Timedelta(hours=lookback)
     lookback_unix = lookback_hours / pd.Timedelta('1s')
 
     df['endbarrier_unix'] = df['unix'] + lookback_unix
-    df = df[['unix','Close','endbarrier_unix','upper_barrier','lower_barrier']]
+    df['endbarrier_dt'] = pd.to_datetime(df['endbarrier_unix'], unit='s')
+    
+    
+    df = df[['unix','Close','endbarrier_unix','upper_barrier','lower_barrier', 'endbarrier_dt']]
     arr = df.to_numpy()
 
     next_arr = np.roll(arr, -1, axis=0)
 
+    # Check if the Close value of the next row is greater than the upper barrier or less than the lower barrier
     upper_touches = (next_arr[:-1, 1] > arr[:-1, 3]) & (next_arr[:-1, 0] <= arr[:-1, 2])
     lower_touches = (next_arr[:-1, 1] < arr[:-1, 4]) & (next_arr[:-1, 0] <= arr[:-1, 2])
 
+    # Create a new array that contains 1 where the upper barrier is touched first, -1 where the lower barrier is touched first, and 0 where the end barrier is reached before either the upper or lower barrier is touched
     labels = np.where(upper_touches, 1, np.where(lower_touches, -1, 0))
+
+    # Append a default label to the end of the labels array
     labels = np.append(labels, 0)
 
+    # Add the labels array as a new column to the original array
     arr = np.column_stack((arr, labels))
 
+    # Add the Close price of the upper barrier touch, lower barrier touch, or end barrier close
     touch_price = np.where(upper_touches, next_arr[:-1, 1], np.where(lower_touches, next_arr[:-1, 1], arr[:-1, 1]))
     touch_price = np.append(touch_price, arr[-1, 1])
-
-    end_barrier_indices = arr[:, 2].astype(int)
-    end_barrier_prices = arr[end_barrier_indices, 1]
-    touch_price = np.where(labels == 0, end_barrier_prices, touch_price)
-
     arr = np.column_stack((arr, touch_price))
 
-    df = pd.DataFrame(arr, columns=['unix','Close','endbarrier_unix','upper_barrier','lower_barrier', 'label', 'touch_price'])
+    df = pd.DataFrame(arr, columns=['unix','Close','endbarrier_unix','upper_barrier','lower_barrier', 'label', 'touch_price', 'endbarrier_dt'])
+    
+    # Find the first index where label == 0
+    zero_label_rows = df[df['label'] == 0]
+
+    # Get the 'endbarrier_dt' values for these rows
+    endbarrier_dt_values = zero_label_rows['endbarrier_dt']
+
+    # Find the first valid index
+    first_valid_endbarrier_dt_index = endbarrier_dt_values.first_valid_index()
+
+    # Get the price at this index
+    price_at_first_valid_endbarrier_dt = df.loc[first_valid_endbarrier_dt_index, 'Close']
+
+    # Replace the 'touch_price' value at this index
+    df.loc[first_valid_endbarrier_dt_index, 'touch_price'] = price_at_first_valid_endbarrier_dt
+
     df.drop('Close', axis =1,inplace=True)
     df.index = date_index
 
