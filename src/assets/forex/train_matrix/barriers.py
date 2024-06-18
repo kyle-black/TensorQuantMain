d1becd40b08dd5e1f663bdc7dbdaa719cf07a89c
kyle-black <kpblack87@gmail.com>
2024-06-18 17:24:28 -0500

    updated barriers

--- src/assets/forex/train_matrix/barriers.py
+++ src/assets/forex/train_matrix/barriers.py
@@ -225,11 +225,16 @@ def calculate_barriers_R(df, lookback):
     df = df[['unix','Close','endbarrier_unix','upper_barrier','lower_barrier']]
     arr = df.to_numpy()
 
-    next_arr = np.roll(arr, -1, axis=0)
+    max_to_right = np.maximum.accumulate(arr[::-1, 1])[::-1]
+    min_to_right = np.minimum.accumulate(arr[::-1, 1])[::-1]
+
+# Check if max or min to the right touches the barriers
+    upper_touches = (max_to_right >= arr[:, 3]) & (arr[:, 0] <= arr[:, 2])
+    lower_touches = (min_to_right <= arr[:, 4]) & (arr[:, 0] <= arr[:, 2])
 
     # Check if the Close value of the next row is greater than the upper barrier or less than the lower barrier
-    upper_touches = (next_arr[:-1, 1] > arr[:-1, 3]) & (next_arr[:-1, 0] <= arr[:-1, 2])
-    lower_touches = (next_arr[:-1, 1] < arr[:-1, 4]) & (next_arr[:-1, 0] <= arr[:-1, 2])
+    #upper_touches = (next_arr[:-1, 1] > arr[:-1, 3]) & (next_arr[:-1, 0] <= arr[:-1, 2])
+    #lower_touches = (next_arr[:-1, 1] < arr[:-1, 4]) & (next_arr[:-1, 0] <= arr[:-1, 2])
     #lower_touches = (next_arr[:-1, 1] < arr[:-1, 4]) & (next_arr[:-1, 0] <= arr[:-1, 2])
 
     # Create a new array that contains 1 where the upper barrier is touched first, -1 where the lower barrier is touched first, and 0 where the end barrier is reached before either the upper or lower barrier is touched
@@ -242,7 +247,7 @@ def calculate_barriers_R(df, lookback):
     arr = np.column_stack((arr, labels))
 
     # Add the Close price of the upper barrier touch, lower barrier touch, or end barrier close
-    touch_price = np.where(upper_touches, next_arr[:-1, 1], np.where(lower_touches, next_arr[:-1, 1], arr[:-1, 1]))
+    touch_price = np.where(upper_touches,arr[:-1, 1], np.where(lower_touches,arr[:-1, 1], arr[:-1, 1]))
     touch_price = np.append(touch_price, arr[-1, 1])
     arr = np.column_stack((arr, touch_price))
 
