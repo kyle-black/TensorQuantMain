e0ffba15a44cd71ea862bba03baedcd00e74de5e
kyle-black <kpblack87@gmail.com>
2024-06-27 19:02:48 -0500

    updated printout

--- src/assets/forex/train_matrix/train_models.py
+++ src/assets/forex/train_matrix/train_models.py
@@ -332,6 +332,8 @@ def ensemble_methods(df, asset, lookback):
     data = []
 
 # Iterate through the zipped lists
+
+    print(type(startprice), type(endprice), type(upperbarrier), type(lowerbarrier), type(Dates), type(enddate))
     for actual, prediction, dwn, neutral, up, start, end, upperbarrier, lowerbarrier, date, end_date in zip(y_test, y_pred, probas[:,0], probas[:,1], probas[:,2], startprice, endprice, upperbarrier, lowerbarrier, Dates, enddate):
         # Append a dictionary for each row of data
         data.append({
