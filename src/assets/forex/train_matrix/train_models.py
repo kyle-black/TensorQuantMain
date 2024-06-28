ea8b8cb0839cca92168e1c89d6741f8204bf288f
kyle-black <kpblack87@gmail.com>
2024-06-28 12:03:36 -0500

    updated predictions

--- src/assets/forex/train_matrix/train_models.py
+++ src/assets/forex/train_matrix/train_models.py
@@ -365,7 +365,7 @@ def ensemble_methods(df, asset, lookback):
     test_data['probs up'] = probas[:,2]
 
     comparison_df.to_csv('comparison_df.csv')
-    predictions_df.to_csv('predictions_data.csv')
+    predictions_df.to_csv('predictions_df.csv')
 
 
 
