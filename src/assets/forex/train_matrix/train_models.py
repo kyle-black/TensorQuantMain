e391cca0b326cb8a39bb81ddca0a3e82d2773476
kyle-black <kpblack87@gmail.com>
2024-06-28 12:19:32 -0500

    updated prinout

--- src/assets/forex/train_matrix/train_models.py
+++ src/assets/forex/train_matrix/train_models.py
@@ -354,9 +354,13 @@ def ensemble_methods(df, asset, lookback):
             'date': date,
             'end_date': end_date
         })
-
+    print('datalist:',data)
 # Convert the list of dictionaries into a DataFrame
     predictions_df = pd.DataFrame(data)
+    print(f'Number of rows in predictions_df: {len(predictions_df)}')
+
+# Optionally, inspect the first few rows to confirm data looks correct
+    print('predictions:',predictions_df.head())
 
    # print(test_data)
     test_data['predictions'] = y_pred
