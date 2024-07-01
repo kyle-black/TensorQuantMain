19ac84f45308aca5b6f67e00e5167f037b7f82b9
kyle-black <kpblack87@gmail.com>
2024-06-30 19:28:09 -0500

    updated train

--- src/assets/forex/train_matrix/train_models.py
+++ src/assets/forex/train_matrix/train_models.py
@@ -166,11 +166,11 @@ def ensemble_methods(df, asset, lookback):
 
    
         
-    train_idx =   train_datasets[-1]
-
+   # train_idx =   train_datasets[-1]
+    train_idx =train_datasets
    # print('train_dataset', len(train_data))
-
-    test_idx = test_datasets[-1]
+    test_dix = test_datasets
+    #test_idx = test_datasets[-1]
 
     print('Test idx:', test_idx)
 
