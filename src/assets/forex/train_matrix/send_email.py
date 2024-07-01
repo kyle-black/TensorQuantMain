620ea0e64c9c657c03a8cf24babdd5ae3adb6fdf
kyle-black <kpblack87@gmail.com>
2024-07-01 14:47:33 -0500

    updated dollar bars

--- src/assets/forex/train_matrix/send_email.py
+++ src/assets/forex/train_matrix/send_email.py
@@ -12,7 +12,7 @@ def run_email():
     password = "tgmq pkpa oquz zhwi"  # Consider using a more secure authentication method
     subject = "Test Data CSV"
     body = "Attached is the test_data.csv file."
-    file_path = "barriercheck.csv"
+    file_path = "predictions_df.csv"
 
     # Create the email message
     msg = EmailMessage()
