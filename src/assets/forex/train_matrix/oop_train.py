5f08e93377560ed8c2e4143c0763285e34521307
kyle-black <kpblack87@gmail.com>
2024-07-02 14:46:34 -0500

    updated send email

--- src/assets/forex/train_matrix/oop_train.py
+++ src/assets/forex/train_matrix/oop_train.py
@@ -254,7 +254,9 @@ def train_data(filtered_df,asset,lookback):
 if __name__ == "__main__":
     asset = "EURUSD"
     df = prepare_data()
+    
+    send_email.run_email()
 
     train_data(df, asset,720)
-    send_email.run_email()
+   # send_email.run_email()
     
