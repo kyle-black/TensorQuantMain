276e80c0c46cd301ea3ca409c247234ea4b1533e
kyle-black <kpblack87@gmail.com>
2024-07-23 09:13:38 -0500

    updated barriers

--- src/assets/forex/train_matrix/barriers.py
+++ src/assets/forex/train_matrix/barriers.py
@@ -371,7 +371,7 @@ def calculate_barriers_R(df, lookback):
 
 def calculate_barriers_R(df, lookback):
     # Calculate volatility
-    df = df[-100000:]
+    #df = df[:]
     df['pct_change'] = df['Close'].pct_change()
     volatility = df['pct_change'].rolling(window=1000).std()
     
