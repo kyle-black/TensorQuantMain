0630e6f640242f87052f1f3722992c1e61c4eb17
kyle-black <kpblack87@gmail.com>
2024-07-23 09:29:56 -0500

    updated train data

--- src/assets/forex/train_matrix/oop_train.py
+++ src/assets/forex/train_matrix/oop_train.py
@@ -222,8 +222,8 @@ def prepare_data():
     print('Applying Triple Barriers:')
     df = L.triple_barriers()
     print(df)
-    return df
-    '''
+    
+    
    # return df
     
    # df.to_parquet('barrier_df.parquet')
@@ -249,7 +249,7 @@ def prepare_data():
     filtered_df.dropna(inplace=True)
     
     return filtered_df
-    '''
+    
     
     
 def train_data(filtered_df,asset,lookback):
