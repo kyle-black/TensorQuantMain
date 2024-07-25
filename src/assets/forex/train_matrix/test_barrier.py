ba22ce60259b29b2c4aa6deefbebee6d9efe78c8
kyle-black <kpblack87@gmail.com>
2024-07-25 10:58:40 -0500

    updated function

--- src/assets/forex/train_matrix/test_barrier.py
+++ src/assets/forex/train_matrix/test_barrier.py
@@ -68,7 +68,7 @@ def calculate_barriers_R(df, lookback):
 
 # Example usage
 
-period =100000
+period =1000000
 data = {
     'Date': pd.date_range(start='2022-01-01', periods=period, freq='h'),
     'Close': np.random.rand(period) * period
@@ -76,5 +76,5 @@ data = {
 df = pd.DataFrame(data)
 lookback = 48  # 24 hours lookback period
 result_df = calculate_barriers_R(df, lookback)
-result_df.to_csv('barrier_pricecheck.csv')
+#result_df.to_csv('barrier_pricecheck.csv')
 print(result_df)
