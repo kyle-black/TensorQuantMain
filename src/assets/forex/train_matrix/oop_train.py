188ed20e618abb8de447a3e1f115336e7ac67749
kyle-black <kpblack87@gmail.com>
2024-08-01 17:54:32 -0500

    filtered set

--- src/assets/forex/train_matrix/oop_train.py
+++ src/assets/forex/train_matrix/oop_train.py
@@ -242,11 +242,11 @@ def prepare_data():
 
     #df = pd.read_parquet('final_df.parquet')
    # fm = FeatureMaker(df, lookback, asset)
-   # tEvents = fm.create_CUMSUM_filter()
-   # filtered_df = df[df.index.isin(tEvents)]
+    tEvents = fm.create_CUMSUM_filter()
+    filtered_df = df[df.index.isin(tEvents)]
     
     #print('filtered_df:',filtered_df)
-    filtered_df = df
+   # filtered_df = df
     #filtered_df =df
     #filtered_df.dropna(inplace=True)
     
