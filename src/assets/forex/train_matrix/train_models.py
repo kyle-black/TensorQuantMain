ab55da799244537b4db48fbed163515d8687ba3b
kyle-black <kpblack87@gmail.com>
2024-08-01 19:00:01 -0500

    updated cols

--- src/assets/forex/train_matrix/train_models.py
+++ src/assets/forex/train_matrix/train_models.py
@@ -67,7 +67,11 @@ def ensemble_methods(df, asset, lookback):
     df['endbarrier_unix'] = pd.to_datetime(df['endbarrier_unix'], unit='s')
 
     prices = df[['Close', 'touch_price', 'Date', 'endbarrier_unix', 'upper_barrier', 'lower_barrier', 'pct']]
-    df.drop(columns=['Date', 'unix', 'endbarrier_unix', 'Volume', 'Close', 'upper_barrier', 'lower_barrier', 'pct_change', 'Datetime', 'touch_price', 'prices_in_range', 'pct', 'Datehold', 'pips','change'], inplace=True)
+    df.drop(columns=['Date', 'unix', 'endbarrier_unix', 'Volume', 'Close', 'upper_barrier', 'lower_barrier', 'pct_change', 'Datetime', 'touch_price', 'prices_in_range', 'pct', 'Datehold', 'pips','change','durableGoods', '15Yr_Fixed',
+       '30Yr_Fixed', 'CPI', 'GDP', 'Production_Total_Index', 'Yields_COD',
+       'consumerSentiment', 'federalFunds', 'inflation', 'inflationRate',
+       'initialClaims', 'nominalPotentialGDP', 'rates_CreditCards', 'realGDP',
+       'realGDPPerCapita', 'retailMoneyFunds', 'retailSales'], inplace=True)
     df.dropna(how='all', inplace=True)
     df['label'] = df['label'].map({-1: 0, 0: 1, 1: 2})
 
