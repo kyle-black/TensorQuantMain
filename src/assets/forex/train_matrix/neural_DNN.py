13061a92cf5760a5d7c22ffeb0444c85edf69a8c
kyle-black <kpblack87@gmail.com>
2024-08-05 12:31:08 -0500

    change test size

--- src/assets/forex/train_matrix/neural_DNN.py
+++ src/assets/forex/train_matrix/neural_DNN.py
@@ -32,7 +32,7 @@ def run_model(df, asset, lookback):
 
     train_datasets, test_datasets = crossvalidation.run_split_process(df)
     #print('traindatasets:',train_datasets)
-    print('testdatasets:',test_datasets[-7])
+    print('testdatasets:',test_datasets[-1])
     feature_cols = df.drop('label', axis=1).columns
     print('columns in training:', feature_cols)
     target_col = 'label'
@@ -40,8 +40,8 @@ def run_model(df, asset, lookback):
     n_components = 15
     scaler = StandardScaler()
 
-    train_idx = train_datasets[-7]
-    test_idx = test_datasets[-7]
+    train_idx = train_datasets[-1]
+    test_idx = test_datasets[-1]
 
     train_data = df.iloc[train_idx]
     test_data = df.iloc[test_idx]
