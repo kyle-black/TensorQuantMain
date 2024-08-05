65dd0be5926b725dcc6af1b3542fdc53929781dc
kyle-black <kpblack87@gmail.com>
2024-08-05 12:43:44 -0500

    updated training

--- src/assets/forex/train_matrix/neural_DNN.py
+++ src/assets/forex/train_matrix/neural_DNN.py
@@ -85,7 +85,7 @@ def run_model(df, asset, lookback):
     )
 
     # Train the model
-    model.fit(X_train, y_train, epochs=10, batch_size=128, validation_split=0.2)
+    model.fit(X_train, y_train, epochs=50, batch_size=128, validation_split=0.2)
 
     # Evaluate the model
     test_loss, test_acc, test_precision, test_recall, test_auc = model.evaluate(X_test, y_test)
