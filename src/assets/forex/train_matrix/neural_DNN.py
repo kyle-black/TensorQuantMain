19fa8b11f39a7fedc7cec8a3d2a572b247b943be
kyle-black <kpblack87@gmail.com>
2024-08-07 12:43:38 -0500

    updated nans

--- src/assets/forex/train_matrix/neural_DNN.py
+++ src/assets/forex/train_matrix/neural_DNN.py
@@ -37,8 +37,9 @@ def run_model(df, asset, lookback, learning_rate=0.001, batch_size=128, epochs=1
     prices = df[['Close', 'touch_price', 'Date', 'endbarrier_unix', 'upper_barrier', 'lower_barrier', 'pct']]
     df = df[['label', 'Middle_Band', 'Upper_Band', 'Lower_Band', 'Log_Returns', 'MACD', 'Signal_Line_MACD', 'RSI', 'Close', 'Volume', '%K',
        '%D', 'daily_return', 'direction', 'volume_direction', 'OBV',
-       'tenkan_sen', 'kijun_sen', 'senkou_span_a',
+       'tenkan_sen', 'kijun_sen', 'senkou_span_a', 'senkou_span_b',
        'chikou_span']]
+    df.dropna(inplace=True)
     print('new_df', df.head())
     df.dropna(how='all', inplace=True)
     df['label'] = df['label'].map({-1: 0, 0: 1, 1: 2})
