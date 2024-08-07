50e446a7e6b0ae4c6695fed4aa48550d898cd409
kyle-black <kpblack87@gmail.com>
2024-08-07 13:42:43 -0500

    updated mask

--- src/assets/forex/train_matrix/barriers.py
+++ src/assets/forex/train_matrix/barriers.py
@@ -550,7 +550,7 @@ def calculate_barriers_R(df, lookback):
     # Function to find prices between unix and endbarrier_unix
     def find_prices_in_range(start_unix, end_unix):
         #mask = (price_df_values[:, 0] >= start_unix) & (price_df_values[:, 0] <= end_unix)
-        mask = (price_df_values[:, 0]) & (price_df_values[:, 0])
+        mask = (price_df_values[:, 0])
         return price_df_values[mask, 1]
 
     # Apply the function to each row
