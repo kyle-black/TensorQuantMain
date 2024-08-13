1b7579ca946aaf211e25b964f8e08347bd6643ec
kyle-black <kpblack87@gmail.com>
2024-08-13 11:31:24 -0500

    fix enviorment

--- src/assets/forex/train_matrix/move_model.py
+++ src/assets/forex/train_matrix/move_model.py
@@ -24,7 +24,7 @@ file_path = 'test_data.csv'  # Path to your file
 space_name = 'financedata'  # Name of your Space
 region = 'nyc3'  # Region of your Space, e.g., 'nyc3'
 access_key = os.environ['access_key']  
-secret_key = os.access_key.environ['secret_key']  # Your Spaces Secret Key
+secret_key = os.environ['secret_key']  # Your Spaces Secret Key
 
 # Upload the file
 upload_file_to_spaces(file_path, space_name, region, access_key, secret_key)
