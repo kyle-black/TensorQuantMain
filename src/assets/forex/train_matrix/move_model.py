5387f00851a49f213516fc087c0c57a271aa2b17
kyle-black <kpblack87@gmail.com>
2024-08-13 11:46:29 -0500

    fix saved models

--- src/assets/forex/train_matrix/move_model.py
+++ src/assets/forex/train_matrix/move_model.py
@@ -20,8 +20,8 @@ def upload_file_to_spaces(file_path, space_name, region, access_key, secret_key)
         client.upload_fileobj(data, space_name, file_path)
 
 # Parameters
-file_path = 'saved_models/EURUSD_1024_1.h5'  # Path to your file
-space_name = 'financedata'  # Name of your Space
+file_path = 'saved_models'  # Path to your file
+space_name = 'saved_models'  # Name of your Space
 region = 'nyc3'  # Region of your Space, e.g., 'nyc3'
 access_key = os.environ['access_key']  
 secret_key = os.environ['secret_key']  # Your Spaces Secret Key
