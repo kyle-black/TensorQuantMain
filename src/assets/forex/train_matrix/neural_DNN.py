de410b2d25316e5058a06999e868147ab196a383
kyle-black <kpblack87@gmail.com>
2024-08-14 13:32:47 -0500

    updated shuffle

--- src/assets/forex/train_matrix/neural_DNN.py
+++ src/assets/forex/train_matrix/neural_DNN.py
@@ -54,8 +54,8 @@ def run_model(df, asset, lookback, learning_rate=0.001, batch_size=32, epochs=10
     n_components = 14
     scaler = StandardScaler()
 
-    train_idx = train_datasets[-1]
-    test_idx = test_datasets[-1]
+    train_idx = train_datasets[-2]
+    test_idx = test_datasets[-2]
 
     train_data = df.iloc[train_idx]
     test_data = df.iloc[test_idx]
