c22f00143b43ae6e4f42cdf360b30e091ce285b1
kyle-black <kpblack87@gmail.com>
2024-08-14 08:55:35 -0500

    updated lookback

--- src/assets/forex/train_matrix/oop_train.py
+++ src/assets/forex/train_matrix/oop_train.py
@@ -201,7 +201,7 @@ def prepare_data():
     
     asset = 'EURUSD'
     dollar_amount = 20000
-    lookback = 40
+    lookback = 10
     #'''
     # Use Dask to read the CSV file in chunks
     raw = dd.read_csv('merged.csv')
@@ -275,6 +275,6 @@ if __name__ == "__main__":
     
    # send_email.run_email()
     df = pd.read_parquet('final_df.parquet')
-    train_data(df, asset,40)
+    train_data(df, asset,10)
  #   send_email.run_email()
     
