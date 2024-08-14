c78409dd2b70a2fb742298a68ecca190b7bd78a7
kyle-black <kpblack87@gmail.com>
2024-08-14 13:45:56 -0500

    updated batch size

--- src/assets/forex/train_matrix/neural_DNN.py
+++ src/assets/forex/train_matrix/neural_DNN.py
@@ -24,7 +24,7 @@ class MonitorActivation(layers.Layer):
 
 
 
-def run_model(df, asset, lookback, learning_rate=0.001, batch_size=32, epochs=1000):
+def run_model(df, asset, lookback, learning_rate=0.001, batch_size=128, epochs=1000):
     if asset is not None:
         asset = asset
 
