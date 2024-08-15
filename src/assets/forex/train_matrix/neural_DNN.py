58db0d5b4002d2840863510f839590f78bf58f94
kyle-black <kpblack87@gmail.com>
2024-08-15 08:55:03 -0500

    fix cols in neural dnn

--- src/assets/forex/train_matrix/neural_DNN.py
+++ src/assets/forex/train_matrix/neural_DNN.py
@@ -42,7 +42,8 @@ def run_model(df, asset, lookback, learning_rate=0.001, batch_size=128, epochs=1
     df = df[['label', 'Middle_Band', 'Upper_Band', 'Lower_Band', 'Log_Returns', 'MACD', 'Signal_Line_MACD', 'RSI', 'Volume', '%K',
        '%D', 'daily_return', 'direction', 'volume_direction', 'OBV',
        'tenkan_sen', 'kijun_sen', 'senkou_span_a', 'senkou_span_b',
-       'chikou_span']]
+       'chikou_span', 'Close_AUDUSD', 'Close_USDCAD',
+       'Close_USDCHF', 'AUDUSD_Returns', 'USDCAD_Returns', 'USDCHF_Returns']]
     
     #df['label'] = df['label'].map({-1: 0, 0: 0, 1: 1})
 
@@ -50,7 +51,7 @@ def run_model(df, asset, lookback, learning_rate=0.001, batch_size=128, epochs=1
     feature_cols = df.drop('label', axis=1).columns
     target_col = 'label'
 
-    n_components = 14
+    n_components = 20
     scaler = StandardScaler()
 
     train_idx = train_datasets[-3]
