012f8d439e9be1bcd2f561848aa242645cab9894
kyle-black <kpblack87@gmail.com>
2024-08-15 09:24:07 -0500

    updated live data pull

--- src/assets/forex/deploy/live_data.py
+++ src/assets/forex/deploy/live_data.py
@@ -5,7 +5,7 @@ import datetime
 
 
 
-def latest_data():
+def latest_data(securities):
     username = 'StoCASHtic-ML'
     password = 'Biobio9034!'
 
@@ -14,26 +14,29 @@ def latest_data():
    # securities_list = ['AUDUSD','EURGBP','EURUSD', 'USDCAD', 'USDCHF','USDCNH','USDHKD','USDJPY']
     #df_all = pd.DataFrame()
 
-    #df_list =[]
+    df_list =[]
     #for security in securities_list:
     
-    security = 'EURUSD'
+    #security = 'EURUSD'
 
-    nifty_index_data = tv.get_hist(symbol=f'{security}',exchange='OANDA',interval=Interval.in_1_minute,n_bars=10000)
+    for i in securities:
+        nifty_index_data = tv.get_hist(symbol=f'{i}',exchange='OANDA',interval=Interval.in_1_minute,n_bars=10000)
 
-    df= nifty_index_data
+        df= nifty_index_data
 
-    df =df.reset_index()
-    df.rename(columns = {'datetime':'Date','open': 'Open', 'high':'High', 'low':'Low', 'close':'Close','volume':'Volume'}, inplace=True)
+        df =df.reset_index()
+        df.rename(columns = {'datetime':f'{i}_Date','open': f'{i}_Open', 'high':f'{i}_High', 'low':f'{i}_Low', 'close':f'{i}_Close','volume':f'{i}_Volume'}, inplace=True)
 
-    #df_list.append(df)
+        df_list.append(df)
     
-    #df_all = pd.concat(df_list, ignore_index =True)
+    df_all = pd.concat(df_list, ignore_index =True)
 
-   # df.set_index('Date', inplace=True)
+    df.set_index('EURUSD_Date', inplace=True)
     return df
 
 
 
-#if __name__ in "__main__":
-#    print(latest_data_60())
+if __name__ in "__main__":
+    security_list = 'EURUSD','AUDUSD', 'CADUSD', 'USDCHF'
+    latest_data(security_list)
+    print(latest_data)
