d0bd0b65c3f91810a5ec34088537715c137754d2
kyle-black <kpblack87@gmail.com>
2024-08-19 13:35:39 -0500

    updated test

--- src/assets/forex/deploy/run_model.py
+++ src/assets/forex/deploy/run_model.py
@@ -26,7 +26,7 @@ def prepare_data( dollar_threshold, asset, window_length, securities):
 
 def make_predictions(pca,scaler, model, dollar_threshold, asset, window_length, securities):
     df = prepare_data(dollar_threshold, asset, window_length,securities)
-
+    print('pre prediction df:', df)
     prediction_df = df.dropna()
 
 
