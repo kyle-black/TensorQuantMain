9034cf23c5f3c40b68b5fd3f8c8cd203e79ef5ac
kyle-black <kpblack87@gmail.com>
2024-08-19 15:24:52 -0500

    updated test to 1000

--- src/assets/forex/train_matrix/trade_simulator.py
+++ src/assets/forex/train_matrix/trade_simulator.py
@@ -12,7 +12,7 @@ def trade_dataframe_creator(df):
     #Randomize Trading sequence
    # random_trade = np.sort(np.random.randint(data_length, size=(600)))
 
-    random_trade = np.random.choice(data_length, size=600, replace=False)
+    random_trade = np.random.choice(data_length, size=100, replace=False)
     random_trade = np.sort(random_trade)
    # random_trade = np.random.randint(data_length, size=(100))
     selected_trades = []
@@ -121,7 +121,7 @@ def trade_simulate(df,account):
 
 #if __name__ in "_main__":
 
-df = pd.read_csv('testfiles/test_result_EURUSD_0816-11.csv')
+df = pd.read_csv('testfiles/test_result_EURUSD_0816-12.csv')
 
 # Set initial account balance
 account = 1000
