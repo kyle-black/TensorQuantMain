f56e7dafcc99771353a23669d3838aeced6ca471
kyle-black <kpblack87@gmail.com>
2024-08-20 14:09:10 -0500

    preduction df cols

--- src/assets/forex/deploy/run_model.py
+++ src/assets/forex/deploy/run_model.py
@@ -28,7 +28,7 @@ def prepare_data( dollar_threshold, asset, window_length, securities):
 
 def make_predictions(pca,scaler, model, dollar_threshold, asset, window_length, securities):
     df = prepare_data(dollar_threshold, asset, window_length,securities)
-    print('pre prediction df:', df)
+    print('pre prediction df:', df.columns)
     prediction_df = df.dropna()
 
 
