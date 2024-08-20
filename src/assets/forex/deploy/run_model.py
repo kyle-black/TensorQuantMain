0eab7d2f1fdc3b5e220fd78225cd7530796a21e7
kyle-black <kpblack87@gmail.com>
2024-08-20 14:42:51 -0500

    updateds to nans

--- src/assets/forex/deploy/run_model.py
+++ src/assets/forex/deploy/run_model.py
@@ -29,7 +29,7 @@ def prepare_data( dollar_threshold, asset, window_length, securities):
 def make_predictions(pca,scaler, model, dollar_threshold, asset, window_length, securities):
     df = prepare_data(dollar_threshold, asset, window_length,securities)
     print('pre prediction df:', df.columns)
-    prediction_df = df.dropna()
+   # prediction_df = df.dropna()
 
     prediction_df = prediction_df.drop(columns=['Date', 'Datehold', 'change', 'pct_change', 'pips'])
 
