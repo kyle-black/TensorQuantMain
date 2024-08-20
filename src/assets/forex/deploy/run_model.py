8ce776a03ce4b359b76614280f71dc2ab52e0c64
kyle-black <kpblack87@gmail.com>
2024-08-20 14:35:14 -0500

    prediction df

--- src/assets/forex/deploy/run_model.py
+++ src/assets/forex/deploy/run_model.py
@@ -33,6 +33,11 @@ def make_predictions(pca,scaler, model, dollar_threshold, asset, window_length, 
 
     prediction_df = prediction_df.drop(columns=['Date', 'Datehold', 'change', 'pct_change', 'pips'])
 
+    prediction_df = [['Middle_Band', 'Upper_Band', 'Lower_Band', 'Log_Returns', 'MACD', 'Signal_Line_MACD', 'RSI', 'Volume', '%K',
+        '%D', 'daily_return', 'direction', 'volume_direction', 'OBV',
+       'tenkan_sen', 'kijun_sen', 'senkou_span_a', 'senkou_span_b',
+       'chikou_span', 'Close_AUDUSD', 'Close','volatility','day_of_week']]
+
 
     ##### Fit scaler
     scaled_df = scaler.transform(prediction_df)
