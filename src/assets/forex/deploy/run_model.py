04799c0897a7d6bd80b9f01c9f52f2457651c70c
kyle-black <kpblack87@gmail.com>
2024-08-20 12:29:27 -0500

    updated file directory

--- src/assets/forex/deploy/run_model.py
+++ src/assets/forex/deploy/run_model.py
@@ -57,13 +57,13 @@ def make_predictions(pca,scaler, model, dollar_threshold, asset, window_length, 
 
 if __name__ in "__main__":
     #Load TF model
-    model = tf.keras.models.load_model('models/EURUSD/EURUSD_0816_20.h5')
+    model = tf.keras.models.load_model('models/EURUSD/EURUSD_0816-20.h5')
 
     # Load scaler
-    scaler = joblib.load('models/EURUSD/EURUSD_0816_20_scaler.pkl')
+    scaler = joblib.load('models/EURUSD/EURUSD_0816-20_scaler.pkl')
 
     # Load PCA
-    pca = joblib.load('models/EURUSD/EURUSD_0816_20_pca.pkl')
+    pca = joblib.load('models/EURUSD/EURUSD_0816-20_pca.pkl')
     #pca =None
     #scaler =None
     #model =None
