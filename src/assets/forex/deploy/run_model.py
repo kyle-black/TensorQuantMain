a3335a7a05bf53c970bca17dbdbb1dc1f6d2a366
kyle-black <kpblack87@gmail.com>
2024-08-21 13:32:58 -0500

    run model

--- src/assets/forex/deploy/run_model.py
+++ src/assets/forex/deploy/run_model.py
@@ -34,6 +34,7 @@ def make_predictions(pca,scaler, model, dollar_threshold, asset, window_length, 
     prediction_df = df.drop(columns=['Date', 'Datehold', 'change', 'pct_change', 'pips'])
     print('predictiondf check',prediction_df) 
     prediction_df.dropna(inplace=True)
+    print(prediction_df.isna())
     prediction_df = df[['Middle_Band', 'Upper_Band', 'Lower_Band', 'Log_Returns', 'MACD', 'Signal_Line_MACD', 'RSI', 'Volume', '%K',
         '%D', 'daily_return', 'direction', 'volume_direction', 'OBV',
        'tenkan_sen', 'kijun_sen', 'senkou_span_a', 'senkou_span_b',
