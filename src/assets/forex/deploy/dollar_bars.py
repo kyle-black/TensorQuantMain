dba4ffceae097bb25c3eb725e7ec74c0225505cf
kyle-black <kpblack87@gmail.com>
2024-08-27 14:11:44 -0500

    updated

--- src/assets/forex/deploy/dollar_bars.py
+++ src/assets/forex/deploy/dollar_bars.py
@@ -6,66 +6,39 @@ from scipy.stats import boxcox
 
 
 
-def dollar_bar_creator(asset,df_,dollar_amt):
-
-   # df_.rename(columns={'date':'Date','open':'Open','low':'Low','high':'High','close':'Close','volume':'Volume'}, inplace=True)
-    print('dollar bar df:', df_)
-    #df_ = pd.read_csv('merged.csv')
-    df_.sort_values('EURUSD_Date', inplace =True)
-    #df_.sort_index()
-
-    df_ = df_[:]
+def dollar_bar_creator(asset, df_, dollar_amt):
+    # Convert the date column to datetime format to avoid sorting issues
+    df_['EURUSD_Date'] = pd.to_datetime(df_['EURUSD_Date'], errors='coerce')
     
+    # Drop rows where the conversion failed (i.e., rows with invalid dates)
+    df_.dropna(subset=['EURUSD_Date'], inplace=True)
+
+    # Sort the DataFrame by date
+    df_.sort_values('EURUSD_Date', inplace=True)
+
     close = f'{asset}_Close'
     volume = f'{asset}_Volume'
-    
-   # df_ = df_.set_index('Date')
-
-    #for i in ['AUDUSD','USDCAD','USDCHF']:
-    #    df_[f'{i}_Returns'] = df_[f'Close_{asset}'].pct_change()
-
-
-   # df_ = df_[[close,volume]]
-
-    #df_ = df_.tail(500000)
-
 
     vol_count = 0
     dollar_count = 0
     new_bar = []
 
-
     for i, d in df_.iterrows():
-
-
-        dollar_count+=round((d[volume] * d[close]),2)
-        #vol_count += d[volume]
+        dollar_count += round((d[volume] * d[close]), 2)
 
         if dollar_count >= dollar_amt:
-            bar ={'Date':i,'Close':d[close],'Volume':d[volume],'AUDUSD_Close':d['AUDUSD_Close']}
+            bar = {'Date': i, 'Close': d[close], 'Volume': d[volume], 'AUDUSD_Close': d['AUDUSD_Close']}
             new_bar.append(bar)
-         #   vol_count = 0 
             dollar_count = 0
-         
-        
-
-
-        
-        #print(dollar_count)
-        #print(i,d)
 
     new_bar_df = pd.DataFrame(new_bar)
 
-    new_bar_df['pips'] = new_bar_df['Close'] *10000
-    new_bar_df['change'] = (new_bar_df['pips'].diff()) 
+    new_bar_df['pips'] = new_bar_df['Close'] * 10000
+    new_bar_df['change'] = new_bar_df['pips'].diff()
     new_bar_df['pct_change'] = new_bar_df['change'].pct_change()
 
     new_bar_df['Datehold'] = pd.to_datetime(new_bar_df['Date'])
     new_bar_df['day_of_week'] = new_bar_df['Datehold'].dt.dayofweek
-   # new_bar_df.drop('Datehold', inplace=True)
-
-   
-
 
     return new_bar_df
 
