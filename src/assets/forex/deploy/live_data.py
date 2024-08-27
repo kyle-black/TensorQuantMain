0e9993430f97a291ea58feeb8c14503aef29b75a
kyle-black <kpblack87@gmail.com>
2024-08-27 11:12:46 -0500

    updated live data

--- src/assets/forex/deploy/live_data.py
+++ src/assets/forex/deploy/live_data.py
@@ -39,57 +39,33 @@ def latest_data(securities):
 
 
 def latest_data(symbol):
-
     dflist = []
-    
-    ###EURUSD #############
-   # for i in secur
-    url ='https://financialmodelingprep.com/api/v3/historical-chart/1min/EURUSD?from=2024-08-27&to=2024-08-27&apikey=3e17d2b777a13feee4c1243985cdc7c4'
 
+    # EURUSD
+    url = 'https://financialmodelingprep.com/api/v3/historical-chart/1min/EURUSD?from=2024-08-27&to=2024-08-27&apikey=3e17d2b777a13feee4c1243985cdc7c4'
     response = requests.get(url)
-
-       
-    print(response.json())
     data = response.json()
-
-
-    df =pd.DataFrame(data)
-
-    df.rename(columns={'date': f'EURUSD_Date', 'open': f'EURUSD_Open', 'high': f'EURUSD_High', 'low': f'EURUSD_Low', 'close': f'EURUSD_Close', 'volume': f'EURUSD_Volume'}, inplace=True)
-
+    df = pd.DataFrame(data)
+    df.rename(columns={'date': 'EURUSD_Date', 'open': 'EURUSD_Open', 'high': 'EURUSD_High', 'low': 'EURUSD_Low', 'close': 'EURUSD_Close', 'volume': 'EURUSD_Volume'}, inplace=True)
     dflist.append(df)
-    print('length of data:', df)            
-    
-    ### AUDUSD ##########
-    aud_url ='https://financialmodelingprep.com/api/v3/historical-chart/1min/AUDUSD?from=2024-08-15&to=2024-08-21&apikey=3e17d2b777a13feee4c1243985cdc7c4'
 
+    # AUDUSD
+    aud_url = 'https://financialmodelingprep.com/api/v3/historical-chart/1min/AUDUSD?from=2024-08-15&to=2024-08-21&apikey=3e17d2b777a13feee4c1243985cdc7c4'
     aud_response = requests.get(aud_url)
-
-       
-    print(aud_response.json())
     aud_data = aud_response.json()
-
-
-    aud_df =pd.DataFrame(aud_data)
-
-    aud_df.rename(columns={'date': f'AUDUSD_Date', 'open': f'AUDUSD_Open', 'high': f'AUDUSD_High', 'low': f'AUDUSD_Low', 'close': f'AUDUSD_Close', 'volume': f'AUDUSD_Volume'}, inplace=True)
+    aud_df = pd.DataFrame(aud_data)
+    aud_df.rename(columns={'date': 'AUDUSD_Date', 'open': 'AUDUSD_Open', 'high': 'AUDUSD_High', 'low': 'AUDUSD_Low', 'close': 'AUDUSD_Close', 'volume': 'AUDUSD_Volume'}, inplace=True)
     dflist.append(aud_df)
 
+    # Combine dataframes
+    df_all = pd.concat(dflist, axis=1)
+    df_all.set_index('EURUSD_Date', inplace=True)
 
-    print('length of data:', aud_df)
-
-
-    
-    df_all = pd.concat(dflist,axis=1)
-        
-    df_all = df_all.set_index('EURUSD_Date')
-            
+    # Writing DataFrame to JSON file
     with open(f'updated_data/{symbol}_live.json', 'a') as f:
-        json.dump(df_all, f)
+        json.dump(df_all.to_dict(orient='records'), f)
 
-    print('df_all:', df_all)
     return df_all
-
 
 
 def combine_data(symbol):
