8f971d44badad1a43636692657926c11ca599987
kyle-black <kpblack87@gmail.com>
2024-09-12 13:19:42 -0500

    updated dollar bars

--- src/assets/forex/train_matrix/oop_train.py
+++ src/assets/forex/train_matrix/oop_train.py
@@ -249,9 +249,9 @@ def train_data(df, asset,lookback,n_components,training_cols,model_num,learning_
 
 if __name__ == "__main__":
     #hyperparameter for experiment and model creation
-    model_num ='EURUSD_0816-23'
+    model_num ='EURUSD_0816-24'
     asset = "EURUSD"
-    dollar_amount =30000
+    dollar_amount =5000
     lookback =10
     n_components = 17
     learning_rate =.0001
