2acd145957200ab896469a83bdab56ac182ec8ce
kyle-black <kpblack87@gmail.com>
2024-09-12 15:19:04 -0500

    updated ip test

--- src/assets/forex/train_matrix/send_email.py
+++ src/assets/forex/train_matrix/send_email.py
@@ -14,7 +14,7 @@ def run_email(model_num):
     subject = "Test Data CSV"
     body = "Attached is the test_data.csv file."
    # file_path = f"test_result_{model_num}.csv"
-    file_path ='test_zip.zip'
+    file_path ='test2.zip'
     # Create the email message
     msg = EmailMessage()
     msg["From"] = sender_email
--- src/assets/forex/train_matrix/trade_simulator.py
+++ src/assets/forex/train_matrix/trade_simulator.py
@@ -100,7 +100,7 @@ def trade_append(df, initial_balance, leverage, base_lot_size):
 
 # Main logic
 if __name__ == "__main__":
-    df = pd.read_csv('testfiles/test_result_EURUSD_0816-26.csv')
+    df = pd.read_csv('testfiles/test_result_EURUSD_0816-27.csv')
 
     # Set initial parameters
     initial_balance = 2000
