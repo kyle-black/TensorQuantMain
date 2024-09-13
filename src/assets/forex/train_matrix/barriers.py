987ecef19198aaa5f6c1b56b8199681787be105e
kyle-black <kpblack87@gmail.com>
2024-09-12 20:20:30 -0500

    updated barriers

--- src/assets/forex/train_matrix/barriers.py
+++ src/assets/forex/train_matrix/barriers.py
@@ -40,12 +40,12 @@ def calculate_barriers_R(df, lookback):
     # Add necessary columns
     df['Datetime'] = pd.to_datetime(df['Date'])
     df['unix'] = df['Datetime'].astype('int64') // 10**9
-    df['upper_barrier'] = df['Close'] * (1 + 5 * df['volatility'])
-    df['lower_barrier'] = df['Close'] * (1 - 5 * df['volatility'])
+    df['upper_barrier'] = df['Close'] * (1 + 6 * df['volatility'])
+    df['lower_barrier'] = df['Close'] * (1 - 6 * df['volatility'])
     
     # Calculate lookback in seconds
     lookback_seconds = (lookback*10) * 3600
-    df['endbarrier_unix'] = df['unix'] + lookback_seconds
+    df['endbarrier_unix'] = df['unix'] + (1.5*lookback_seconds)
 
     # Copy df to price_df and keep only 'unix' and 'Close'
     price_df = df[['unix', 'Close']].copy()
