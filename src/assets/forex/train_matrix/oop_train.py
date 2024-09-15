4b9bad55ab6542ca7b219943b8798e25d7dbae65
kyle-black <kpblack87@gmail.com>
2024-09-15 16:18:30 -0500

    updated test size

--- src/assets/forex/train_matrix/oop_train.py
+++ src/assets/forex/train_matrix/oop_train.py
@@ -249,7 +249,7 @@ def train_data(df, asset,lookback,n_components,training_cols,model_num,learning_
 
 if __name__ == "__main__":
     #hyperparameter for experiment and model creation
-    model_num ='EURUSD_0816-35'
+    model_num ='EURUSD_0816-36'
     asset = "EURUSD"
     dollar_amount =10000
     lookback =10
