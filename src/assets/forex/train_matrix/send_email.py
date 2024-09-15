f7e380deb3a6b0eaedf71acf1b0c4355d8801b6f
kyle-black <kpblack87@gmail.com>
2024-09-15 11:23:50 -0500

    rerun

--- src/assets/forex/train_matrix/send_email.py
+++ src/assets/forex/train_matrix/send_email.py
@@ -13,8 +13,8 @@ def run_email(model_num):
     password = "tgmq pkpa oquz zhwi"  # Consider using a more secure authentication method
     subject = "Test Data CSV"
     body = "Attached is the test_data.csv file."
-    #file_path = f"test_result_{model_num}.csv"
-    file_path = "test_result_EURUSD_0816-31.csv"
+    file_path = f"test_result_{model_num}.csv"
+   # file_path = "test_result_EURUSD_0816-31.csv"
    # file_path ='test2.zip'
     # Create the email message
     msg = EmailMessage()
