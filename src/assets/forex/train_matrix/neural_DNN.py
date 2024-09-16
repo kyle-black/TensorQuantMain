0e9f6634dca581da459f75191dbe5f6d6fa6cb98
kyle-black <kpblack87@gmail.com>
2024-09-16 11:10:14 -0500

    ncompontes 12

--- src/assets/forex/train_matrix/neural_DNN.py
+++ src/assets/forex/train_matrix/neural_DNN.py
@@ -198,7 +198,7 @@ def run_model(df, asset, lookback, n_components, training_cols, model_num, learn
     feature_cols = df.drop('label', axis=1).columns
     target_col = 'label'
 
-    n_components = 5
+    n_components = 12
     scaler = StandardScaler()
 
     train_idx = train_datasets[-1]
