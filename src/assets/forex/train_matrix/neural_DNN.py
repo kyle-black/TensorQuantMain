65e497b7a52e68ca4ebae38a22b900b3334dd72f
kyle-black <kpblack87@gmail.com>
2024-09-16 17:34:52 -0500

    adjust lr

--- src/assets/forex/train_matrix/neural_DNN.py
+++ src/assets/forex/train_matrix/neural_DNN.py
@@ -108,7 +108,7 @@ def run_model(df, asset, lookback, n_components, training_cols, model_num, learn
     )
 
     early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=20, restore_best_weights=True)
-    lr_scheduler = tf.keras.callbacks.ReduceLROnPlateau(monitor='loss', factor=0.5, patience=5, min_lr=1e-6)
+    lr_scheduler = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=5, min_lr=1e-6)
 
     model.fit(X_train, y_train_onehot, epochs=epochs, batch_size=batch_size, validation_split=0.2, callbacks=[early_stopping, lr_scheduler])
 
