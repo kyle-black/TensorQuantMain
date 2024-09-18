436f6dcfb74c6c5b9ae2defbd6a4bcb7b3156f1f
kyle-black <kpblack87@gmail.com>
2024-09-18 11:19:35 -0500

    updated crossval

--- src/assets/forex/train_matrix/oop_train.py
+++ src/assets/forex/train_matrix/oop_train.py
@@ -255,7 +255,7 @@ if __name__ == "__main__":
     #hyperparameter for experiment and model creation
     model_num ='EURUSD_0816-58'
     asset = "EURUSD"
-    dollar_amount =10000
+    dollar_amount =5000
     lookback =10
     n_components = 17
     learning_rate =.0001
