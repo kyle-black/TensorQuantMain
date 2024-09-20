33ddec0df5448f65204691ca768d86da0a34a2b3
kyle-black <kpblack87@gmail.com>
2024-09-20 12:08:05 -0500

    Model 61 -1

--- src/assets/forex/train_matrix/trade_simulator.py
+++ src/assets/forex/train_matrix/trade_simulator.py
@@ -100,7 +100,7 @@ def trade_append(df, initial_balance, leverage, base_lot_size):
 
 # Main logic
 if __name__ == "__main__":
-    df = pd.read_csv('testfiles/test_result_EURUSD_0916-60.csv')
+    df = pd.read_csv('testfiles/test_result_EURUSD_0916-61.csv')
 
     # Set initial parameters
     initial_balance = 2000
