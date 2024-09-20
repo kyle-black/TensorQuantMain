7d6868bf43d4593e633e54100976cfc9ed0e9b9a
kyle-black <kpblack87@gmail.com>
2024-09-20 13:09:12 -0500

    move model change

--- src/assets/forex/deploy/move_model.py
+++ src/assets/forex/deploy/move_model.py
@@ -3,7 +3,6 @@ from botocore.client import Config
 import os
 
 
-
 def upload_file_to_spaces(file_path, space_name, region, access_key, secret_key):
     # Create a session using your DigitalOcean Spaces credentials
     session = boto3.session.Session()
@@ -11,26 +10,29 @@ def upload_file_to_spaces(file_path, space_name, region, access_key, secret_key)
     # Create a client to interact with DigitalOcean Spaces
     client = session.client('s3',
                             region_name=region,
-                            endpoint_url=f'https://financedata.{region}.digitaloceanspaces.com',
+                            endpoint_url=f'https://{space_name}.{region}.digitaloceanspaces.com',
                             aws_access_key_id=access_key,
                             aws_secret_access_key=secret_key)
 
+    # Extract file name from file path
+    file_name = os.path.basename(file_path)
+
     # Upload the file
     with open(file_path, 'rb') as data:
-        client.upload_fileobj(data, space_name, file_path)
+        client.upload_fileobj(data, space_name, file_name)
 
-
+    print(f"{file_name} uploaded successfully to {space_name} in {region}.")
 
 # Upload the file
 
-if __name__ in "main":
+if __name__ == "__main__":
     # Parameters
     file_path = 'models/EURUSD/EURUSD_0916-61.h5'  # Path to your file
     space_name = 'saved_models'  # Name of your Space
     region = 'nyc3'  # Region of your Space, e.g., 'nyc3'
     access_key = os.environ['access_key']  
     secret_key = os.environ['secret_key']  # Your Spaces Secret Key
+    
+    # Call the function to upload the file
     upload_file_to_spaces(file_path, space_name, region, access_key, secret_key)
-
-
 
