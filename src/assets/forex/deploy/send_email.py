0b6dc3711427068a59d01262aeedd646087aa71a
kyle-black <kpblack87@gmail.com>
2024-09-20 13:26:04 -0500

    send meail pca

--- src/assets/forex/deploy/send_email.py
+++ src/assets/forex/deploy/send_email.py
@@ -13,7 +13,7 @@ def run_email():
     password = "tgmq pkpa oquz zhwi"  # Consider using a more secure authentication method
     subject=" model 61 attached"
     body = "."
-    file_path = "models/EURUSD/EURUSD_0916-61.h5"
+    file_path = "models/EURUSD/EURUSD_0916-61_pca.pkl"
 
     # Create the email message
     msg = EmailMessage()
